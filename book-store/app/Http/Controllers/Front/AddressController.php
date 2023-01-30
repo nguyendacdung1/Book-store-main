5ccad0ba91eab876ab5a3b23a48e3d6002d3539e
nguyendacdung1 <nguyendungmk9@gmail.com>
2023-01-30 19:13:28 +0700

    book

---
+++ book-store/app/Http/Controllers/Front/AddressController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\UserAdress;
+use App\Models\UserCart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $totalCart = UserCart::where('user_id',Auth::id())->sum('total');
+        $categories = Category::all();
+        $addresses = UserAdress::where('user_id',Auth::id())->orderBy('default','desc')->get();
+        return view('front.account.address.index',compact('totalCart','categories','addresses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        $totalCart = UserCart::where('user_id',Auth::id())->sum('total');
+        return view('front.account.address.create',compact('categories','totalCart'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['user_id'] = Auth::id();
+        if(isset($data['default']) || count(Auth::user()->userAdresses)==0){
+            $data['default']=1;
+            UserAdress::where('user_id',Auth::id())->update(['default'=>0]);
+        }else{
+            $data['default']=0;
+        }
+
+        UserAdress::create($data);
+        return redirect('./account/address');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $categories = Category::all();
+        $totalCart = UserCart::where('user_id',Auth::id())->sum('total');
+        $address = UserAdress::find($id);
+        return view('front.account.address.edit',compact('categories','totalCart','address'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        if(isset($data['default'])){
+            $data['default']=1;
+            UserAdress::where('user_id',Auth::id())->update(['default'=>0]);
+        }else{
+            $data['default']=0;
+        }
+        UserAdress::where('id',$id)->update($data);
+        return redirect('./account/address');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $address =  UserAdress::find($id);
+        if($address->default==1 && Auth::user()->userAdresses->count()>1){
+            $setAddressDefault =  Auth::user()->userAdresses->where('default',0)->first();
+            $setAddressDefault->default = 1;
+            $setAddressDefault->save();
+        }
+        $address->delete();
+        return  redirect('./account/address');
+    }
+}
---
+++ book-store/app/Http/Controllers/Front/CartController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\UserCart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function index(){
+        $totalCart = UserCart::where('user_id',Auth::id())->sum('total');
+        $categories = Category::all();
+        $cart_products = Auth::user()->cart;
+        return view('front.shop.cart',compact('categories','cart_products','totalCart'));
+    }
+
+    public function add($id){
+        $userId = Auth::id();
+        $data=[
+            'user_id'=>$userId,
+            'product_id'=>$id,
+            'price'=>Product::find($id)->price,
+            'total'=>Product::find($id)->price,
+            'qty'=>1
+        ];
+        if(count(Auth::user()->cart->where('product_id',$id))>0){
+            $cart = Auth::user()->cart->where('product_id',$id)->first();
+            $cart->qty = $cart->qty+1;
+            $cart->total = $cart->qty*$cart->price;
+            $cart->save();
+            return 0;
+        }
+        $cart = UserCart::create($data);
+        return 1;
+    }
+    public function delete($id){
+        UserCart::find($id)->delete();
+        return 1;
+    }
+
+    public  function update(Request $request){
+        $id = $request->id;
+        $itemCart = UserCart::find($id);
+        $itemCart->qty = $request->qty;
+        $itemCart->total = $request->qty * $itemCart->price;
+        $itemCart->save();
+        return back();
+    }
+    public function postAdd(Request $request){
+        $id = $request->id;
+        $userId = Auth::id();
+        $data=[
+            'user_id'=>$userId,
+            'product_id'=>$id,
+            'qty'=>$request->qty,
+            'price'=>Product::find($id)->price,
+            'total'=>Product::find($id)->price
+        ];
+        if(count(Auth::user()->cart->where('product_id',$id))==0){
+            $cart = UserCart::create($data);
+        }else{
+            $cart = Auth::user()->cart->where('product_id',$id)->first();
+            $cart->qty = $cart->qty+1;
+            $cart->total = $cart->qty*$cart->price;
+            $cart->save();
+        }
+        return redirect('./cart');
+    }
+}
